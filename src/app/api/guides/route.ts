d1d96d2b267d80da85175e4a34ab6bc67ee676e8
elian bourdu <elian.bourdu@gmail.com>
2024-04-11 23:34:15 +0200

    first authenticated route, fix bcrypt with node gyp, fix potential assert failure

--- src/app/api/guides/route.ts
+++ src/app/api/guides/route.ts
@@ -2,6 +2,7 @@ import {GuideRepository} from "@/server/repositories/guide/guide_repository";
 import {NextRequest, NextResponse} from "next/server";
 import Joi from "joi";
 import {EntityAlreadyExists} from "@/server/errors/entity_already_exists";
+import {getUserFromToken} from "@/server/services/auth";
 
 // définition du schema Joi pour une première validation des données
 // issues du payload de la requête POST
@@ -16,7 +17,7 @@ const schema = Joi.object({
     .required(),
 })
 
-//
+// get all guides
 export async function GET() {
   return GuideRepository.getAllGuides()
     .then(guideList => {
@@ -25,16 +26,23 @@ export async function GET() {
       })
     })
     .catch((error: Error) => {
-      return NextResponse.json({error: error.message}, {status: 500})
+      return NextResponse.json({ error: error.message }, { status: 500 })
     })
 }
 
 // Requête de création de Guide
 export async function POST(request: NextRequest) {
+  const user = await getUserFromToken(request.cookies.get('mechaToken')?.value)
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+
   // récupération du payload
   const bodyPayload = await request.json()
+
   // validation du payload via le schéma Joi définit précédemment
-  const { error, value } = schema.validate(bodyPayload)
+  const { error, value } = schema.validate(bodyPayload, { allowUnknown: false })
+
   if (error) {
     return NextResponse.json({ error }, { status: 400 })
   }
@@ -50,7 +58,7 @@ export async function POST(request: NextRequest) {
         throw new EntityAlreadyExists()
       }
 
-      return GuideRepository.createGuide(title, content, request.user.id)
+      return GuideRepository.createGuide(title, content, user.user__id)
     })
     .then((guide) => {
       return NextResponse.json({ data: guide }, { status: 201 })
--- src/server/services/auth.ts
+++ src/server/services/auth.ts
@@ -1,12 +1,14 @@
-import { hash, compare } from 'bcrypt'
-import { sign } from 'jsonwebtoken'
-import assert from 'assert'
+import { hash, compare } from 'bcryptjs'
+import { sign, verify } from 'jsonwebtoken'
 import {PasswordNotMatchingException} from "@/server/errors/password_not_matching";
+import {NextResponse} from "next/server";
 
 const SALT_ROUND = +process.env.SALT_ROUND ?? 10
 const JWT_SECRET = process.env.JWT_SECRET
 
-assert(JWT_SECRET)
+if (!JWT_SECRET) {
+  throw new Error('JWT_SECRET is not defined')
+}
 
 export async function hashPassword(password: string): Promise<string> {
   return hash(password, SALT_ROUND)
@@ -24,4 +26,33 @@ export async function createJWT(payload: Record<string, any>): Promise<string> {
   return sign(payload, JWT_SECRET, {
     expiresIn: '1h'
   })
+}
+
+export async function verifyJWT(token: string): Promise<Record<string, any>> {
+  return new Promise((resolve, reject) => {
+    return verify(token, JWT_SECRET, (error, decoded) => {
+      if (error) {
+        return reject(error)
+      }
+
+      return resolve(decoded as Record<string, any>)
+    })
+  })
+}
+
+export async function getUserFromToken(token: string): Promise<Record<string, any>|null> {
+  // si le token n'est pas présent, on renvoie une erreur 401
+  if (!token) {
+    return null
+  }
+
+  // vérification du token
+  const payload = await verifyJWT(token).catch(() => null);
+
+  // si le token n'est pas valide, on renvoie une erreur 401
+  if (!payload) {
+    return null
+  }
+
+  return payload
 }
