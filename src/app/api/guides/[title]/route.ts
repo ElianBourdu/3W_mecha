882e8a85499880d0536a3aeeb6940767bc27a40b
elian bourdu <elian.bourdu@gmail.com>
2024-04-13 12:59:49 +0200

    feat: premier jet front style
    feat: styling guides and new guides, wysiwyg component
    
    Revert "feat: styling guides and new guides, wysiwyg component"
    
    This reverts commit 48f0f68c512f45fe653f440d879373f21fe15322.
    
    Reapply "feat: styling guides and new guides, wysiwyg component"
    
    This reverts commit 318cbb82402451ea6d2981b16a2323bd605201c0.

--- src/app/api/guides/[title]/route.ts
+++ src/app/api/guides/[title]/route.ts
@@ -1,15 +1,23 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import {UserRepository} from "@/server/repositories/iam/user_repository";
 import {EntityNotFoundException} from "@/server/errors/not_found";
-import {isUUID} from "@/server/validators/is_uuid";
 import {GuideRepository} from "@/server/repositories/guide/guide_repository";
 
 export async function GET(request: NextRequest, { params }: { params: { title: string } }) {
+  const includes = request.nextUrl.searchParams.getAll('includes') ?? []
+
   return GuideRepository.getGuideByTitle(params.title)
     .then((guide) => {
-      return Response.json({
-        data: guide.toJson()
-      })
+      if (includes.includes('user')) {
+        return UserRepository.getUserById(guide.user__id).then(user => {
+          guide.user = user
+          return guide.toJson()
+        })
+      }
+      return guide.toJson()
+    })
+    .then((guide) => {
+      return Response.json({ data: guide })
     })
     .catch((error) => {
       if (error instanceof EntityNotFoundException) {
--- src/app/guides/[title]/page.tsx
+++ src/app/guides/[title]/page.tsx
@@ -1,7 +1,9 @@
 import {IGuide} from "@/server/entities/guide/guide";
+import styles from "./page.module.css";
+import {IUser} from "@/server/entities/iam/user";
 
-async function getGuideByTitle(title: string): Promise<IGuide> {
-  return fetch(`http://localhost:3000/api/guides/${title}`)
+async function getGuideByTitle(title: string): Promise<IGuide & { user: IUser }> {
+  return fetch(`http://localhost:3000/api/guides/${title}?includes=user`)
     .then(res => res.json())
     .then(json => json.data)
 }
@@ -10,10 +12,10 @@ export default async function GuideByTitle({ params }: {params: {title: string}}
   const guide = await getGuideByTitle(params.title)
 
   return (
-    <div>
-      <h2>{ guide.title }</h2>
-      <p>Guide rédigé par { guide.guide__id }</p>
-      <p>{ guide.content }</p>
+    <div className={styles.guide_container}>
+      <h1>{ guide.title }</h1>
+      <p>Guide rédigé par { guide.user.username }</p>
+      <p dangerouslySetInnerHTML={{__html: guide.content}}></p>
     </div>
   )
 }
