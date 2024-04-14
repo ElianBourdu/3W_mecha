6fa0e45ca9304d9d6e4682f05043f6b7be7048aa
elian bourdu <elian.bourdu@gmail.com>
2024-04-14 17:56:29 +0200

    feat: footer, searchbar refacto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,12 @@ export default async function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         { user ? user.username : '' }
+
+      {/*
+       1 - timer du prochain tournoi
+       2 - CTA vers liste tournois
+       3 -
+       */}
       </div>
     </main>
   );
