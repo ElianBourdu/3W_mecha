7c873cb6367497fb8e0e3e473c8591791d1a0980
elian bourdu <elian.bourdu@gmail.com>
2024-06-22 23:00:34 +0200

    fix: pg connection ssl

--- src/server/db.js
+++ src/server/db.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
+  ssl: process.env.POSTGRES_SSL === 'true'
 });
 
 export function getPool() {
